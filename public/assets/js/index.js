9b8639cd5c0ec2d4d0d6bf7ed27853a580e34bca
Anthony Mitchell <j.ant.mitch@gmail.com>
2018-05-31 18:32:16 -0400

    update to index.js

---
+++ public/assets/js/index.js
@@ -0,0 +1,29 @@
+$(".submit").on("click", function(event){
+    event.preventDefault();
+    window.location = "userInputs.html";
+    
+    //Grabbing user input
+     var userInfo = {
+       username: $("#username").val().trim(),
+       password: $("#password").val().trim()
+     };
+     console.log(userInfo);
+  
+  $.post("....", userInfo, function(data){
+    if(data) {
+      console.log("user added")
+    }
+    else {
+        if (errors) {
+            var message = "You must provide a username and password";
+            $('#alertModal').find('.modal-body p').text(message);
+            $('#alertModal').modal('show')
+        }
+    }
+  
+    //clears fields
+    $("#username").val("");
+    $("#password").val();
+  });
+  
+  });
