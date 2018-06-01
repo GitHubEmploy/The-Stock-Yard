b036a99c1b5ea408aa76e124fc6ae3b8b49dcac9
Michael Dibble <mdibble93@gmail.com>
2018-05-31 21:20:05 -0400

    added bar graph to userinput.js

--- public/assets/js/UserInput.js
+++ public/assets/js/UserInput.js
@@ -1,24 +1,53 @@
-$(".submit").on("click", function(event){
-    event.preventDefault();
-    
-    //Grabbing user input
-     var newHoldings = {
-       positon: $("#tickerSymbol").val().trim(),
-       qty: $("#qty").val().trim()
-     };
-     console.log(newHoldings);
-  
-  $.post("....", newHoldings, function(data){
-    if(data) {
-      alert("added");
+$(document).ready(function() {
+
+    var barGraphData = {
+        // Empty ticker symbols array where symbols will be pushed to when "#submit" is clicked
+        tickerSymbols: [],
+        // Empty array for symbols quantities where quantities will be pushed to when "#submit" is clicked
+        quantities: []
     }
-    else {
-      alert("something went wrong");
-    }
-  
-    //clears fields
-    $("#tickerSymbol").val("");
-    $("#qty").val();
-  });
-  
-  });
+
+    // Creating the bar graph by getting the element by id "#myCanvas"
+    var ctx = $("#myCanvas");
+    // Creating a "graph object" to store the data that is being put in by a user
+    var graph = new Graph(ctx, {
+        // The type of graph being created is a bar graph
+        type: 'bar',
+        // data attribute that contains the "barGraphData"
+        data: barGraphData,
+        //Options that can be added into the bar graph
+        options: {
+            responsive: true,
+            title: {
+                display: true,
+                text: "Chart.js Bar Graph"
+            }
+        }
+    });
+
+    $(".submit").on("click", function(event){
+        event.preventDefault();
+        
+        //Grabbing user input
+         var newHoldings = {
+           positon: $("#tickerSymbol").val().trim(),
+           qty: $("#qty").val().trim()
+         };
+         console.log(newHoldings);
+      
+         // URL
+      $.post("....", newHoldings, function(data){
+        if(data) {
+          alert("added");
+        }
+        else {
+          alert("something went wrong");
+        }
+      
+        //clears fields
+        $("#tickerSymbol").val("");
+        $("#qty").val();
+      });
+
+      }); 
+});
