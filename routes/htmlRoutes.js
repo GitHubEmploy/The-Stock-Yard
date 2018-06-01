b62888054e95fde48b1e3dddf220461f0655340a
Anthony Mitchell <j.ant.mitch@gmail.com>
2018-05-31 21:04:46 -0400

    updates to connection, routes, sequelize, models, handlebars, npm packages

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,9 @@
+module.exports = function(app) {
+    app.get("/second", function(req, res) {
+      res.render("secondary");
+    });
+  
+    app.get("/", function(req, res) {
+      res.render("index");
+    });
+  };
