eaca9bb109aeb6818d5f3ddb55eaee97d862a6b4
Anthony Mitchell <j.ant.mitch@gmail.com>
2018-06-05 18:25:21 -0400

    update to calculate math and convert to currency

--- public/assets/js/UserInput.js
+++ public/assets/js/UserInput.js
@@ -1,61 +1,65 @@
 $(document).ready(function() {
-    var closePrice = [];
-    var closeDate = [];
-    // Creating the br graph from "myCanvas" in secondary.handlebars
-    var barGraph = $('#myCanvas');
-    var graphData= {
-        
-        datasets: [{
-            label: "Close Price",
-            backgroundColor: 'lightblue',
-            data: closePrice
-        }]
-    };
+  var closePrice = [];
+  var closeDate = [];
+  var recentClosePrice = "";
+  // Creating the br graph from "myCanvas" in secondary.handlebars
+  var barGraph = $("#myCanvas");
+  var graphData = {
+    datasets: [
+      {
+        label: "Close Price",
+        backgroundColor: "lightblue",
+        data: closePrice
+      }
+    ]
+  };
 
-    function createNewGraph() {
-        var newGraph = new Chart(barGraph, {
-            type: 'bar',
-            data: graphData,
-            options:{
-                responsive: false,
-                scales: {
-                    xAxes:[{
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Date(YYYY/MM/DD/)',
-                            fontSize: 20,
-                            fontColor: 'white'
-                        },
-                        gridLines: {
-                            color: 'white'
-                        },
-                        ticks: {
-                            fontColor: 'white'
-                        }
-                    }],
-                    yAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Close Price($)',
-                            fontSize: 20,
-                            fontColor: 'white'
-                        },
-                        gridLines: {
-                            color: 'white',
-                            
-                        },
-                        ticks: {
-                            fontColor: 'white',
-                            beginAtZero: true
-                        }
-                    }]
-                }
+  function createNewGraph() {
+    var newGraph = new Chart(barGraph, {
+      type: "bar",
+      data: graphData,
+      options: {
+        responsive: false,
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "Date(YYYY/MM/DD/)",
+                fontSize: 20,
+                fontColor: "white"
+              },
+              gridLines: {
+                color: "white"
+              },
+              ticks: {
+                fontColor: "white"
+              }
             }
-        });
-    }
+          ],
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "Close Price($)",
+                fontSize: 20,
+                fontColor: "white"
+              },
+              gridLines: {
+                color: "white"
+              },
+              ticks: {
+                fontColor: "white",
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
+  }
 
- $("#submit").on("click", function(event) {
-
+  $("#submit").on("click", function(event) {
     event.preventDefault();
     //Grabbing user input
     var newHoldings = {
@@ -77,48 +81,45 @@ $(document).ready(function() {
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-
-
       console.log(response);
       for (var i = 0; i < response.length; i++) {
-        console.log(response[i].date); 
+        console.log(response[i].date);
         console.log(response[i].close);
         closeDate.push(response[i].date);
         closePrice.push(response[i].close);
         createNewGraph();
       }
+      // URL
+      $.post("/second", newHoldings, function(res) {
+        // console.log(res);
+      }).catch(function(err) {
+        throw err;
+      });
 
+      //clears fields
+      $("#tickerSymbol").val("");
+      $("#qty").val("");
+      var value = closePrice.length - 1;
+      console.log(value);
+      console.log(response[value].close);
+      var worthPrice = response[value].close;
+      var stockName = newHoldings.stock_name.toUpperCase();
+      var math = parseInt(newHoldings.qty) * parseInt(worthPrice);
+      var worth = numeral(math).format("$0,0.00");
 
-      for (var i = 0; i < response.length; i++) {
-        //   console.log(response[i].date);
-        closeDate.push(response[i].date);
+      function newCard() {
+        $("#stockTable > tbody").append(
+          "<tr><td>" +
+            stockName +
+            "</td><td>" +
+            newHoldings.qty +
+            "</td><td>" +
+            worth +
+            "</td><tr>"
+        );
       }
 
+      newCard();
     });
-    // URL
-    $.post("/second", newHoldings, function(res) {
-      // console.log(res);
-    }).catch(function(err) {
-      throw err;
-    });
-
-    //clears fields
-    $("#tickerSymbol").val("");
-    $("#qty").val("");
-
-    var worthPrice = closePrice[0];
-    var stockName = newHoldings.stock_name.toUpperCase();
-    var worth = parseInt(newHoldings.qty) * parseInt(worthPrice);
-    console.log(worthPrice);
-    function newCard () {
-        $("#stockTable > tbody").append("<tr><td>" + stockName + "</td><td>" + newHoldings.qty + "</td><td>" + worth + "</td><tr>")
-  
-        console.log("this function ran");
-
-        }
-
-   
-        newCard();
   });
-  
 });
